e17ae6bc43eb983774572e9e6c0ca2f11858b918
gabrysiam8 <gabrysiamiedlar@o2.pl>
2019-11-01 21:38:45 +0100

    Add custom user details service tests

---
+++ src/test/java/com/springboot/rest/quizmania/service/CustomUserDetailsServiceTest.java
@@ -0,0 +1,58 @@
+package com.springboot.rest.quizmania.service;
+
+import com.springboot.rest.quizmania.domain.CustomUser;
+import com.springboot.rest.quizmania.repository.UserRepository;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CustomUserDetailsServiceTest {
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Mock
+    private UserRepository userRepository;
+
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Before
+    public void setUp() {
+        customUserDetailsService = new CustomUserDetailsService(userRepository);
+    }
+
+    @Test
+    public void shouldLoadUser() {
+        //given
+        String username = "test";
+        CustomUser user = CustomUser.builder()
+                         .email("test@gmail.com")
+                         .username(username)
+                         .password("pass")
+                         .role("USER")
+                         .build();
+        when(userRepository.findByEmail(username)).thenReturn(null);
+        when(userRepository.findByUsername(username)).thenReturn(user);
+
+        //when
+        UserDetails result = customUserDetailsService.loadUserByUsername(username);
+
+        //then
+        verify(userRepository, times(1)).findByEmail(anyString());
+        verify(userRepository, times(1)).findByUsername(anyString());
+        assertNotNull(result);
+        assertEquals(user.getUsername(), result.getUsername());
+    }
+}
